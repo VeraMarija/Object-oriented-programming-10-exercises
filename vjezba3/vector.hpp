116913acbf7e1cea5a87abe09943c1001db8029f
VeraMarija <vg48765@student.oss.unist.hr>
2021-11-05 15:13:40 +0100

    upload

---
+++ vjezba3/vector.hpp
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include<iomanip>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void input_vect1(vector<int>& v, int n);
+
+void input_vect2(vector<int>& v, int a, int b);
+
+void output_vect(vector<int>& v, int n);
+
+
+#endif
+
+
+
+
+
+
+
